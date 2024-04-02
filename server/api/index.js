8d1f7ff063be5bf0f7322232869c9f2a919b75ab
Raviraj2022 <ravirajsahu312@gmail.com>
2024-04-02 16:43:24 +0530

    Database connection with server.

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
 
+mongoose
+  .connect(
+    "mongodb+srv://ravirajsahu312:" +
+      encodeURIComponent("Ravi@1234") +
+      "@me-blog.jgy4nyt.mongodb.net/?retryWrites=true&w=majority&appName=me-blog"
+  )
+  .then(() => {
+    console.log("MongoDB is Connnected ");
+  })
+  .catch((err) => console.error("DB Error :", err));
 const app = express();
 
 app.listen(3000, () => {
