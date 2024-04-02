0c4d51f2d70f49ab821d40a24ce08988a27a1679
Raviraj2022 <ravirajsahu312@gmail.com>
2024-04-02 16:55:07 +0530

    create user model.

--- server/api/index.js
+++ server/api/index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 
+const uri =
+  "mongodb+srv://ravirajsahu312:" +
+  encodeURIComponent("Ravi@1234") +
+  "@me-blog.jgy4nyt.mongodb.net/?retryWrites=true&w=majority&appName=me-blog";
 mongoose
-  .connect(
-    "mongodb+srv://ravirajsahu312:" +
-      encodeURIComponent("Ravi@1234") +
-      "@me-blog.jgy4nyt.mongodb.net/?retryWrites=true&w=majority&appName=me-blog"
-  )
+  .connect(uri)
   .then(() => {
     console.log("MongoDB is Connnected ");
   })
