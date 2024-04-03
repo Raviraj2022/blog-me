d43e6c1b8433894189cace8f94c607c6d93b5568
Raviraj2022 <ravirajsahu312@gmail.com>
2024-04-03 10:27:20 +0530

    create a test API route.

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import userRoute from "./routes/user.route.js";
 
 const uri =
   "mongodb+srv://ravirajsahu312:" +
@@ -16,3 +17,4 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+app.use("/api/user", userRoute);
