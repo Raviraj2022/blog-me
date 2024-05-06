f2538fe3ff834cfeb73b8a1915644f0e4794d23e
Raviraj2022 <ravirajsahu312@gmail.com>
2024-05-06 09:14:17 +0530

    Complete about about and projects pages...

--- client-side/src/pages/About.jsx
+++ client-side/src/pages/About.jsx
@@ -1,7 +1,41 @@
-
-
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-5">
+            About Ravi Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Ravi Sharma is a philanthropist in pursuit of building an India of
+              his dreams where there is a balance between the life of rich and
+              poor. Through Prama Jyoti Foundation, he has pledged his life for
+              the mission of “Spreading Goodness” in society by identifying,
+              appreciating those who are living a life of goodness and inspiring
+              others to do so.
+            </p>
+            <p>
+              Ravi Sharma is a philanthropist in pursuit of building an India of
+              his dreams where there is a balance between the life of rich and
+              poor. Through Prama Jyoti Foundation, he has pledged his life for
+              the mission of “Spreading Goodness” in society by identifying,
+              appreciating those who are living a life of goodness and inspiring
+              others to do so.
+            </p>
+            <p>
+              Ravi Sharma is a philanthropist in pursuit of building an India of
+              his dreams where there is a balance between the life of rich and
+              poor. Through Prama Jyoti Foundation, he has pledged his life for
+              the mission of “Spreading Goodness” in society by identifying,
+              appreciating those who are living a life of goodness and inspiring
+              others to do so.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
